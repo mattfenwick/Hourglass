3d3be0cf3096f25ff7570ca68f9004fdc77f8fc0
Matt Fenwick <matt.fenwick@hudl.com>
2015-03-27 12:30:55 -0500

    Initial Commit

---
+++ Hourglass/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Hourglass
+//
+//  Created by Matthew Fenwick on 3/27/15.
+//  Copyright (c) 2015 Matthew Fenwick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
