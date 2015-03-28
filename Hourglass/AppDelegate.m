2da091fa24181d195e3897d32f9e9227263451f8
Matt Fenwick <matt.fenwick@hudl.com>
2015-03-28 09:00:50 -0500

    lifecycle logging

--- Hourglass/AppDelegate.m
+++ Hourglass/AppDelegate.m
@@ -1,40 +1,42 @@
 #import "AppDelegate.h"
 
+
 @interface AppDelegate ()
 
 @end
 
-@implementation AppDelegate
 
+@implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    NSLog(@"did finish launching");
     return YES;
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application
 {
-
+    NSLog(@"will resign active");
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
 {
-
+    NSLog(@"did enter background");
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application
 {
-
+    NSLog(@"will enter foreground");
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
-
+    NSLog(@"did become active");
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
 {
-
+    NSLog(@"will terminate");
 }
 
 @end
